5621f3a3ed017bafe4cb0c7954da27c6d6e45697
Aaron Moss <r0jol@unb.ca>
2012-07-14 14:40:24 -0300

    Fixed isomorphism check to check angles, not constraints
    * also uses fuzzy equality to account for floating point error

--- test/isomorph_check.m
+++ test/isomorph_check.m
@@ -1,3 +1,4 @@
+bas_q_gram;
 img_mat;
 cob_imgs;
 
@@ -10,10 +11,11 @@ endfunction;
 
 function [cMat] = cmp_mat(Mat1, Mat2)
 	[n_m, d_m] = size(Mat1);
-	cMat = [ num2str(Mat1 == Mat2) repmat(" | ",n_m,1) num2str(Mat1) repmat(" | ",n_m,1) num2str(Mat2) ];
+	cMat = [ num2str(abs(Mat1 - Mat2) <= 1.0e-16) repmat(" | ",n_m,1) num2str(Mat1) repmat(" | ",n_m,1) num2str(Mat2) ];
 endfunction;
 
 B = inv(aug_mat(M(s,:)));
+Q = q_gram(M, q_mat(M));
 
 [vn, vd] = size(vs);
 
@@ -21,10 +23,11 @@ for i = 1:vn
 	v = vs(i,:);
 	T = B * aug_mat(M(v,:));
 	Mt = (T * M')';
-	if ( all(M == Mt) )
+	Qt = q_gram(Mt, q_mat(Mt));
+	if ( all(all(abs(Q - Qt) <= 1.0e-16)) )
 		disp(["PASS     " gs(i,:)]);
 	else
 		disp(["    FAIL " gs(i,:)]);
-		disp([ repmat("         ",n,1) cmp_mat(M,Mt) ]);
+		disp([ repmat("         ",n,1) cmp_mat(Q,Qt) ]);
 	end;
 end;
